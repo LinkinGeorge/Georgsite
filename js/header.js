09bd39228b022609c974ef165abdbb9695306e0e
LinkinGeorge <georgdonner98@gmail.com>
2017-03-25 22:24:11 +0000

    Changed to fixed header on desktop too.

--- js/header.js
+++ js/header.js
@@ -1,19 +1,7 @@
 $(document).ready(function () {
-
-    if (window.matchMedia('(max-width: 767px)').matches) {
-        $('.navbar').addClass('navbar-fixed-top');
-        $('#content').addClass('fix-top-margin');
-    } else {
-        $('.navbar').removeClass('navbar-fixed-top');
-        $('#content').removeClass('fix-top-margin');
-    }
-    $(window).on('resize',function(){
-        if (window.matchMedia('(max-width: 767px)').matches) {
-            $('.navbar').addClass('navbar-fixed-top');
-            $('#content').addClass('fix-top-margin');
-        } else {
-            $('.navbar').removeClass('navbar-fixed-top');
-            $('#content').removeClass('fix-top-margin');
-        }
+    $('.grid').masonry({
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: 200
     });
 })
