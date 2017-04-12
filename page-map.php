8799c4bef80cd025576b37189f553a19d1e493ab
LinkinGeorge <georgdonner98@gmail.com>
2017-04-12 17:29:09 +0100

    Removed locations context in page php file.

--- page-map.php
+++ page-map.php
@@ -6,6 +6,4 @@
 $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
-$query = 'pagename=locations';
-$context['locations'] = Timber::query_post($query);
 Timber::render( 'map.twig', $context );
